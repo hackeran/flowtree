e6b7d15ce76c298aec4ec8e27384e36d8eab6019
bmenrigh@ucsd.edu <bmenrigh@ucsd.edu@56f185a6-52e4-1186-f399-59b3468c50e4>
2011-01-31 04:58:23 +0000

    Adding the base files.  The AVL tree will be used for the de-duplication.  The program does nothing but return 0 at this point.

---
+++ trunk/flowtree.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+#include "pavl.h"
+
+/* Function prototypes */
+int main(int, char * const []);
+
+
+int main(int argc, char * const argv[]) {
+
+
+  return 0;
+}
